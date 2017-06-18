f18a6ed6cfaa35d2a7030483676d54edffe57d3c
刘祺 <3442853561@qq.com>
2017-06-18 13:28:46 +0800

    Update 0.0.3

--- src/examples/main.rs
+++ src/examples/main.rs
@@ -1,9 +1,10 @@
 extern crate xiangyun;
 
-use xiangyun::rand::{Rand, Style};
+use xiangyun::{Rand, Style};
 
 fn main() {
     let mut foo = Rand::new(Style::Lazy);
+    foo.srand(3);
     println!("Style::Lazy");
     println!("{}", foo.get_rand());
     println!("{}", foo.get_rand());
@@ -19,8 +20,12 @@ fn main() {
     println!("Style::BMrand");
     println!("{}", foo.get_rand());
     println!("{}", foo.get_rand());
-	let mut foo = Rand::new(Style::Marsaglia);
+    let mut foo = Rand::new(Style::Marsaglia);
     println!("Style::Marsaglia");
+    println!("{}", foo.get_rand());
+    println!("{}", foo.get_rand());
+    let mut foo = Rand::new(Style::Crand);
+    println!("Style::Crand");
     println!("{}", foo.get_rand());
     println!("{}", foo.get_rand());
 }
