2df1fb7cfc2e0c979d24b7bb0e2e8dbbf3be89d1
刘祺 <3442853561@qq.com>
2017-06-18 17:22:49 +0800

    Add rand!

--- src/examples/main.rs
+++ src/examples/main.rs
@@ -1,31 +1,25 @@
+#[macro_use]
 extern crate xiangyun;
 
 use xiangyun::{Rand, Style};
 
 fn main() {
-    let mut foo = Rand::new(Style::Lazy);
-    foo.lazy_srand();
     println!("Style::Lazy");
-    println!("{}", foo.get_rand());
-    println!("{}", foo.get_rand());
-    let mut foo = Rand::new(Style::PMrand);
+    let foo = rand!(2);
+	println!("{}, {}", foo[0], foo[1]);
     println!("Style::PMrand");
-    println!("{}", foo.get_rand());
-    println!("{}", foo.get_rand());
-    let mut foo = Rand::new(Style::Gauss);
+    let foo = rand!(Style::PMrand, 2);
+	println!("{}, {}", foo[0], foo[1]);
     println!("Style::Gauss");
-    println!("{}", foo.get_rand());
-    println!("{}", foo.get_rand());
-    let mut foo = Rand::new(Style::BMrand);
+    let foo = rand!(Style::Gauss, 2);
+	println!("{}, {}", foo[0], foo[1]);
     println!("Style::BMrand");
-    println!("{}", foo.get_rand());
-    println!("{}", foo.get_rand());
-    let mut foo = Rand::new(Style::Marsaglia);
+    let foo = rand!(Style::BMrand, 2);
+	println!("{}, {}", foo[0], foo[1]);
     println!("Style::Marsaglia");
-    println!("{}", foo.get_rand());
-    println!("{}", foo.get_rand());
-    let mut foo = Rand::new(Style::Crand);
+    let foo = rand!(Style::Marsaglia, 2);
+	println!("{}, {}", foo[0], foo[1]);
     println!("Style::Crand");
-    println!("{}", foo.get_rand());
-    println!("{}", foo.get_rand());
+    let foo = rand!(Style::Crand, 2);
+	println!("{}, {}", foo[0], foo[1]);
 }
--- src/rand.rs
+++ src/rand.rs
@@ -52,22 +52,30 @@ impl Rand {
                 }
             }
         }
-        self.seed = seed;
+        if seed > 0 {
+            match self.style {
+                Style::Lazy | Style::PMrand => self.seed = seed,
+				Style::Crand => self.seed = 1,
+                _ => self.seed = seed >> 32,
+            }
+        } else {
+            self.seed = 1;
+        }
     }
 
     pub fn get_rand(&mut self) -> f64 {
         match self.style {
             Style::PMrand => {
                 self.seed = Rand::pmrand(self.seed, 48271);
-                self.seed as f64 / RAND_MAX
+                self.seed as f64 / RAND_MAX.abs()
             }
-            Style::Gauss => Rand::gauss(&mut self.seed, 25) / RAND_MAX,
-            Style::BMrand => Rand::bmrand(&mut self.seed, &mut self.attachment) / RAND_MAX,
-            Style::Marsaglia => Rand::marsaglia(&mut self.seed, &mut self.attachment) / RAND_MAX,
-            Style::Crand => Rand::crand(&mut self.seed) as f64 / RAND_MAX,
+            Style::Gauss => (Rand::gauss(&mut self.seed, 25) / RAND_MAX).abs(),
+            Style::BMrand => (Rand::bmrand(&mut self.seed, &mut self.attachment) / RAND_MAX).abs(),
+            Style::Marsaglia => (Rand::marsaglia(&mut self.seed, &mut self.attachment) / RAND_MAX).abs(),
+            Style::Crand => (Rand::crand(&mut self.seed) as f64 / RAND_MAX).abs(),
             _ => {
                 self.seed = Rand::lazy(self.seed);
-                self.seed as f64 / RAND_MAX
+                (self.seed as f64 / RAND_MAX).abs()
             }
         }
     }
@@ -96,6 +104,7 @@ impl Rand {
 
     fn bmrand(seed: &mut i64, attachment: &mut Vec<f64>) -> f64 {
         // FIXME: value assigned to `z` is never read
+		#[allow(unused_assignments)]
         let mut z = 0.0;
         if attachment.len() == 0 {
             attachment.push(0.0);
@@ -118,6 +127,7 @@ impl Rand {
 
     fn marsaglia(seed: &mut i64, attachment: &mut Vec<f64>) -> f64 {
         // FIXME: value assigned to `x` is never read
+		#[allow(unused_assignments)]
         let mut x = 0.0;
         if attachment.len() == 0 {
             attachment.push(0.0);
@@ -151,7 +161,39 @@ impl Rand {
     }
 
     fn crand(seed: &mut i64) -> i64 {
-        *seed = *seed * 1103515245 + 12345;
+		*seed = *seed * 1103515245 + 12345;
         (*seed >> 16) & (RAND_MAX as i64)
     }
+}
+
+#[macro_export]
+macro_rules! rand {
+    () => {{
+        let mut foo = Rand::new(Style::Lazy);
+        foo.lazy_srand();
+        foo.get_rand()
+    }};
+	($num: expr) => {{
+		let mut foo = Rand::new(Style::Lazy);
+		let mut bar: Vec<f64> = Vec::new();
+        foo.lazy_srand();
+		for _ in 0..$num {
+			bar.push(foo.get_rand());
+		}
+		bar
+	}};
+    ($style: path) => {{
+        let mut foo = Rand::new($style);
+        foo.lazy_srand();
+        foo.get_rand()
+    }};
+	($style: path, $num: expr) => {{
+		let mut foo = Rand::new($style);
+		let mut bar: Vec<f64> = Vec::new();
+        foo.lazy_srand();
+		for _ in 0..$num {
+			bar.push(foo.get_rand());
+		}
+		bar
+	}};
 }
